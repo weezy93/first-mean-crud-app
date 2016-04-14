4e37e3d8b2e38697cf9cb99d034683f8364b99f3
Louisa Reese <louisa.e.reese@gmail.com>
2016-04-14 10:47:30 -0600

    updated production variable

--- src/_config.js
+++ src/_config.js
@@ -3,7 +3,9 @@ var config = {};
 config.mongoURI = {
   test: 'mongodb://localhost/first-mean-app-testing',
   development: 'mongodb://localhost/first-mean-app',
-  production: process.env.mongoURI
+  production: process.env.MONGODB_URI
 }
 
 module.exports = config;
+
+// MONGODB_URI: mongodb://heroku_2g3tmv18:kqfc5rn6ff11ud106lbeg3alib@ds023540.mlab.com:23540/heroku_2g3tmv18
