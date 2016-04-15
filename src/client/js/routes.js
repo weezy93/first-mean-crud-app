aa5fbf6e40ac021a0f1979e14fa12a761848e917
Louisa Reese <louisa.e.reese@gmail.com>
2016-04-15 10:52:39 -0600

    breaking out index to main and partials on client side

--- src/client/js/routes.js
+++ src/client/js/routes.js
@@ -2,7 +2,8 @@ angular.module('studentApp')
 .config(function ($routeProvider) {
   $routeProvider
   .when('/', {
-
+    templateUrl: '../partials/main.html',
+    controller: 'addStudentController'
   })
   .when('/students', {
 
