88e2f4df8071891b80aaee4915bf51e963fc3a60
Louisa Reese <louisa.e.reese@gmail.com>
2016-04-15 15:23:36 -0600

    login sets token properly and redirects properly

--- src/client/js/routes.js
+++ src/client/js/routes.js
@@ -1,19 +1,43 @@
 angular.module('studentApp')
-.config(function ($routeProvider, $locationProvider) {
+.config(function ($routeProvider, $locationProvider, $httpProvider) {
   $routeProvider
   .when('/', {
     templateUrl: '../partials/main.html',
-    controller: 'addStudentController'
+    controller: 'addStudentController',
+    restricted: true,
+    preventLoggedIn: false
   })
   .when('/register', {
     templateUrl: '../partials/signup.html',
     controller: 'signUpController',
+    restricted: false,
+    preventLoggedIn: true,
   })
   .when('/login', {
     templateUrl: '../partials/login.html',
     controller: 'loginController',
+    restricted: false,
+    preventLoggedIn: true
   })
-  .when('/logout', {})
+  .when('/logout', {
+    restricted: false,
+    preventLoggedIn: false
+  })
   .otherwise('/');
   $httpProvider.interceptors.push('authInterceptor');
+});
+
+angular.module('studentApp').run( function ($rootScope, $location, $window, authService) {
+
+  $rootScope.$on('$routeChangeStart', function (event, next, current) {
+    // next is route we're going to
+    // if restricted and no token
+    if (next.restricted && !$window.localStorage.getItem('token')) {
+      $location.path('/login');
+    }
+
+    if (next.preventLoggedIn && $window.localStorage.getItem('token')) {
+      $location.path('/');
+    }
+   });
 });
--- src/client/js/services.js
+++ src/client/js/services.js
@@ -100,11 +100,27 @@ angular.module('studentApp')
       $window.localStorage.setItem('token', JSON.stringify(userData.data.data.token));
     },
     getUserInfo: function (userData) {
-      $window.localStorage.get('user');
+      $window.localStorage.getItem('user');
     }
 
   }
 }])
-.service('authInterceptor', ['$scope', function ($scope) {
-  
+.service('authInterceptor', ['$window', '$q', function ($window, $q) {
+  return {
+    // always make sure to return anything you use here!
+    request: function(config) {
+      // check for token in headers
+      // config.headers['X-requested-with'] = XMLHttpRequest;
+      var token = $window.localStorage.getItem('token');
+      if (token) {
+        config.headers.Authorization = 'Bearer' + token;
+      }
+      return config;
+    },
+    responseError: function(err){
+      // if header auth is not present throw an error
+      console.log('response err', err);
+      return err;
+    }
+  };
 }]);
